547c9263bcea630230f1faccb62c8ee162a735ad
Min Van <min@asl19.org>
2023-09-05 16:54:53 +0700

    Update dependences
    - Change yarn to npm
    - Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
+// @ts-expect-error (doesn’t need typing)
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 const eslintConfig = {
   extends: ["@asl-19/eslint-config", "@asl-19/eslint-config/react"],
+  plugins: ["sort-keys-fix"],
   rules: {
     "no-restricted-imports": "off",
+    "sort-keys-fix/sort-keys-fix": [
+      "warn",
+      "asc",
+      {
+        caseSensitive: false,
+        natural: true,
+      },
+    ],
   },
 };
 
-// eslint-disable-next-line functional/immutable-data
 module.exports = eslintConfig;
