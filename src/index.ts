6c32b71881da43bda2b79d76cbd74b81596ff07a
Grant Heaslip <grant@asl19.org>
2025-08-14 12:57:06 -0400

    Use module: "preserve" (safe as long as consuming packages are ESM); add .js file extensions (so compiled files are valid ESM and usable without bundler)

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@
  * @packageDocumentation
  */
 
-export type { default as FormState } from "./FormState";
-export type { default as StylableFC } from "./StylableFC";
-export { default as useAddHydratedClassName } from "./useAddHydratedClassName";
-export { default as useDynamicFocusOutlines } from "./useDynamicFocusOutlines";
-export { default as useFocusElementAfterRender } from "./useFocusElementAfterRender";
-export { default as useFormStateAndFocusManagement } from "./useFormStateAndFocusManagement";
-export { default as useOnClickOutside } from "./useOnClickOutside";
+export type { default as FormState } from "./FormState.js";
+export type { default as StylableFC } from "./StylableFC.js";
+export { default as useAddHydratedClassName } from "./useAddHydratedClassName.js";
+export { default as useDynamicFocusOutlines } from "./useDynamicFocusOutlines.js";
+export { default as useFocusElementAfterRender } from "./useFocusElementAfterRender.js";
+export { default as useFormStateAndFocusManagement } from "./useFormStateAndFocusManagement.js";
+export { default as useOnClickOutside } from "./useOnClickOutside.js";
